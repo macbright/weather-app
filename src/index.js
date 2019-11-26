8d3883c94a20a6a2d8aa94e57aaabb9239e08af0
bright okike <brightokike@gmail.com>
2019-11-26 13:30:34 +0300

    linter error fix

--- src/index.js
+++ src/index.js
@@ -1,55 +1,27 @@
 
-/*global window, localStorage */
-/*eslint no-undef: "error"*/
+/* global window, localStorage */
+/* eslint no-undef: "error" */
+/* eslint prefer-arrow-callback: "error" */
+/* eslint-disable no-use-before-define: "error" */
+/* eslint-disable indent: "error" */
+// eslint-disable-next-line no-undef
 const $ = document.querySelector.bind(document);
 
 
-const getApi = () => {
-	const city = $('#city-name').value;
-	const country = $('#country').value;
-	return `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=19162a83776adb533575bcaa858e87a0`;
-}
 
-
-$('#submit').addEventListener('click', (e) =>{
-	e.preventDefault();
-	const url = getApi();
-	fetch(url, {mode: 'cors'})
-	.then(function(response) {
-		return response.json();
-	}).then(function(response){
-		
-		localStorage.setItem('city', JSON.stringify(response));
-		display();
-	}).catch(err => {
-		console.log("Error Reading data " + err);
-	});
-})
-
-const display = () => {
-	clearNodes();
-	changeBg();
-	const weatherData = JSON.parse(localStorage.getItem('city'));
-	cityname(weatherData.name, weatherData.weather[0].icon, weatherData.timezone);
-	degrees( weatherData.main.temp, weatherData.weather[0].icon);
-	latitute(weatherData.coord.lon, weatherData.coord.lat);
-	sys(weatherData.sys, weatherData.weather[0].icon);
-	mainData(weatherData.main, weatherData.weather[0].icon);
-	weather(weatherData.weather[0], weatherData.weather[0].icon);
-}
-
-const weatherimage = (icon) =>{
+const weatherimage = (icon) => {
 	const img = document.createElement('img');
 	img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 	return img;
-}
+};
 
-const timezone = (data) =>{
+const timezone = (data) => {
 	const timezone = document.createElement('h2');
-	timezone.innerHTML = `GMT +${data/3600}`;
+	timezone.innerHTML = `GMT +${data / 3600}`;
 	$('.town-name').appendChild(timezone);
-}
-const cityname = (data, image, time) =>{
+};
+
+const cityname = (data, image, time) => {
 	const city = document.createElement('h2');
 	city.innerHTML = data;
 	city.classList.add('city-name');
@@ -58,21 +30,21 @@ const cityname = (data, image, time) =>{
 	const img = weatherimage(image);
 	img.classList.add('image');
 	$('.town-name').appendChild(img);
-}
+};
 
-const degrees = (k, image) =>{
+const degrees = (k, image) => {
 	const c = convertToCelsius(k);
 	const temp = document.createElement('h3');
-	temp.innerHTML = `${c} &#8451`; 
+	temp.innerHTML = `${c} &#8451`;
 	$('.degrees').appendChild(temp);
 	const img = weatherimage(image);
 	img.classList.add('image');
 	$('.degrees').appendChild(img);
-}
+};
 
 const convertToCelsius = (k) => {
 	return parseFloat((k - 273.15).toFixed(6));
-}
+};
 
 const latitute = (long, lati) => {
 	const lon = document.createElement('h3');
@@ -81,17 +53,18 @@ const latitute = (long, lati) => {
 	lat.innerHTML = `lat: ${lati}&#176`;
 	$('.sunset').appendChild(lat);
 	$('.sunset').appendChild(lon);
-}
+};
+
 const sys = (data, image) => {
 	const img = weatherimage(image);
 	img.classList.add('image');
 	$('.sys').appendChild(img);
 	for (const [key, value] of Object.entries(data)) {
-		const p = document.createElement('p')
+		const p = document.createElement('p');
 		p.innerHTML = `${key}:  ${value}`; 
-		$('.sys').appendChild(p)
+		$('.sys').appendChild(p);
 	}
-}
+};
 
 const mainData = (data, image) => {
 	const img = weatherimage(image);
@@ -99,10 +72,10 @@ const mainData = (data, image) => {
 	$('.main-data').appendChild(img);
 	for (const [key, value] of Object.entries(data)) {
 		const p = document.createElement('p');
-		p.innerHTML = `${key}:  ${value}`; 
+		p.innerHTML = `${key}:  ${value}`;
 		$('.main-data').appendChild(p);
 	}
-}
+};
 
 const weather = (data, image) => {
 	const img = weatherimage(image);
@@ -113,10 +86,10 @@ const weather = (data, image) => {
 	$('.weather').appendChild(heading);
 	for (const [key, value] of Object.entries(data)) {
 		const p = document.createElement('p');
-		p.innerHTML = `${key}:  ${value}`; 
+		p.innerHTML = `${key}:  ${value}`;
 		$('.weather').appendChild(p);
 	}
-}
+};
 
 const clearNodes = () => {
 	$('.town-name').innerHTML = '';
@@ -125,25 +98,57 @@ const clearNodes = () => {
 	$('.sys').innerHTML = '';
 	$('.main-data').innerHTML = '';
 	$('.weather').innerHTML = '';
-
 }
 
 const changeBg = () => {
 	const weatherData = JSON.parse(localStorage.getItem('city'));
 	const sky = weatherData.weather[0].main;
-
-	if(sky === 'Clear'){
-		$('body').style.backgroundImage = "url(../dist/img/weather.jpg)";
-	} else if(sky === 'Snow'){
-		$('body').style.backgroundImage = "url(../dist/img/snow.gif)";
-	}else if(sky === 'Rain'){
-		$('body').style.backgroundImage = "url(../dist/img/rain.gif)";
+	if (sky === 'Clear') {
+		$('body').style.backgroundImage = 'url(../dist/img/weather.jpg)';
+	} else if (sky === 'Snow') {
+		$('body').style.backgroundImage = 'url(../dist/img/snow.gif)';
+	}else if (sky === 'Rain') {
+		$('body').style.backgroundImage = 'url(../dist/img/rain.gif)';
 	} else if (sky === 'Clouds') {
-		$('body').style.backgroundImage = "url(../dist/img/cloudy.gif)";
+		$('body').style.backgroundImage = 'url(../dist/img/cloudy.gif)';
 	} else {
-		$('body').style.backgroundImage = "url(../dist/img/weather.jpg)";
+		$('body').style.backgroundImage = 'url(../dist/img/weather.jpg)';
 	}
 
 }
 
-display()
+const getApi = () => {
+	const city = $('#city-name').value;
+	const country = $('#country').value;
+	return `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=19162a83776adb533575bcaa858e87a0`;
+}
+
+
+$('#submit').addEventListener('click', (e) => {
+	e.preventDefault();
+	const url = getApi();
+	fetch(url, { mode: 'cors' })
+	.then(function (response) {
+		return response.json();
+	}).then(function (response) {
+		localStorage.setItem('city', JSON.stringify(response));
+		display();
+	}).catch(err => {
+		return ('Error Reading data ' + err);
+	});
+});
+
+const display = () => {
+	clearNodes();
+	changeBg();
+	const weatherData = JSON.parse(localStorage.getItem('city'));
+	cityname(weatherData.name, weatherData.weather[0].icon, weatherData.timezone);
+	degrees(weatherData.main.temp, weatherData.weather[0].icon);
+	latitute(weatherData.coord.lon, weatherData.coord.lat);
+	sys(weatherData.sys, weatherData.weather[0].icon);
+	mainData(weatherData.main, weatherData.weather[0].icon);
+	weather(weatherData.weather[0], weatherData.weather[0].icon);
+};
+
+
+display();
